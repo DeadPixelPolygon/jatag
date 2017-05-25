6aa666b52f2362bedd3a983a17ff3db153d1de67
DeadPixelPolygon <deadpixel.polygon@Gmail.com>
2017-05-25 20:52:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+"""
+		  J.A.T.A.G
+Just Another Text Adventure Game
+
+Written by Matias Z with python 3.x
+Adventure game using Randomat
+"""
+
+#imports
+from randomat import *		#imports the engine
+
+class player:
+	def __init__ (self, px, py):
+		player.self = self
+		player.px = 0
+		player.py = 0
+
+	engine()
